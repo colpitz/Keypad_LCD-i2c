f9dc1aba1516771077138e4f5e3299bc37da3370
colpitz <pedrada@wanadoo.es>
2017-06-06 14:16:29 +0100

    Update keypad.ino

--- keypad.ino
+++ keypad.ino
@@ -1,7 +1,11 @@
 #include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Three columns
+LiquidCrystal_I2C lcd(0x26, 16, 2);
+
+const byte ROWS = 4; // Cuatro filas
+const byte COLS = 4; // Cuatro columnas
 // Define the Keymap
 char keys[ROWS][COLS] = {
   {'1','2','3','A'},
@@ -10,37 +14,25 @@ char keys[ROWS][COLS] = {
   {'#','0','*','D'}
 };
 // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 4, 5, 6, 7 };
+byte rowPins[ROWS] = { 11, 10, 9, 8 };
 // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 8, 9, 10, 11 }; 
-
+byte colPins[COLS] = { 7, 6, 5, 4 }; 
 // Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-#define ledpin 13
-
 void setup()
 {
-  pinMode(ledpin,OUTPUT);
-  digitalWrite(ledpin, HIGH);
-  Serial.begin(9600);
+ lcd.init();
+ lcd.backlight();
+  
 }
 
 void loop()
 {
+   lcd.setCursor(0, 1);
   char key = kpd.getKey();
   if(key)  // Check for a valid key.
   {
-    switch (key)
-    {
-      case '*':
-        digitalWrite(ledpin, LOW);
-        break;
-      case '#':
-        digitalWrite(ledpin, HIGH);
-        break;
-      default:
-        Serial.println(key);
-    }
-  }
+         lcd.print(key);  
+  } 
 }
