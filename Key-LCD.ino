5d0bff9731166fcc0960e86c34d46c6941e9a298
colpitz <pedrada@wanadoo.es>
2017-06-06 12:23:55 +0100

    Update Key-LCD.ino

--- Key-LCD.ino
+++ Key-LCD.ino
@@ -1,7 +1,11 @@
 #include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Three columns
+LiquidCrystal_I2C lcd(0x26, 16, 2);
+
+const byte ROWS = 4; // Cuatro filas
+const byte COLS = 4; // Cuatro columnas
 // Define the Keymap
 char keys[ROWS][COLS] = {
   {'1','2','3','A'},
@@ -13,34 +17,35 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = { 4, 5, 6, 7 };
 // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
 byte colPins[COLS] = { 8, 9, 10, 11 }; 
-
 // Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-#define ledpin 13
-
 void setup()
 {
-  pinMode(ledpin,OUTPUT);
-  digitalWrite(ledpin, HIGH);
-  Serial.begin(9600);
+ lcd.init();
+ lcd.backlight();
+  
+ Serial.begin(9600);
 }
 
 void loop()
 {
+   lcd.setCursor(0, 1);
   char key = kpd.getKey();
   if(key)  // Check for a valid key.
   {
     switch (key)
     {
       case '*':
-        digitalWrite(ledpin, LOW);
+        lcd.print(key);
         break;
       case '#':
-        digitalWrite(ledpin, HIGH);
+       lcd.print(key);
         break;
       default:
         Serial.println(key);
     }
   }
+ 
+
 }
