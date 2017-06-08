82a5fded3b20c7d7232541141b9fb805f3d0774e
colpitz <pedrada@wanadoo.es>
2017-06-08 10:49:21 +0100

    Create Key-LCD.ino

--- Key-LCD.ino
+++ Key-LCD.ino
@@ -12,7 +12,7 @@ char keys[ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'*','0','.','D'}
 };
 // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
 byte rowPins[ROWS] = { 11, 10, 9, 8 };
@@ -21,6 +21,9 @@ byte colPins[COLS] = { 7, 6, 5, 4 };
 // Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 char key;
+
+float valorFreq// valor de la frecuencia con muneros decimales
+  
 void setup()
 {
  lcd.init();
